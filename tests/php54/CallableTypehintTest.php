75878616f7a54f39405e3350fdd4520abc6f7599
Luis Cordova <cordoval@gmail.com>
2013-07-27 02:44:06 -0500

    Update CallableTypehintTest.php

--- tests/php54/CallableTypehintTest.php
+++ tests/php54/CallableTypehintTest.php
@@ -4,7 +4,8 @@ namespace galapagos\php54;
 
 use galapagos\AbstractTestCase;
 
-class CallableTypehintTest extends AbstractTestCase {
+class CallableTypehintTest extends AbstractTestCase
+{
     /** @dataProvider provideTests */
     public function test_transform($name, $code, $expected) {
         $this->assertSame(
