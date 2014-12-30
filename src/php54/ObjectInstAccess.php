c32c1ff01bae982051ebef2e2c95e2abb4b54568
Endel Dreyer <endel.dreyer@gmail.com>
2014-12-29 23:17:55 -0200

    improve object instant access on PropertyFetch expressions.

--- src/php54/ObjectInstAccess.php
+++ src/php54/ObjectInstAccess.php
@@ -4,9 +4,12 @@ namespace galapagos\php54;
 
 class ObjectInstAccess extends \PHPParser_NodeVisitorAbstract {
     public function enterNode(\PHPParser_Node $node) {
-        if (($node instanceof \PHPParser_Node_Expr_MethodCall || $node instanceof \PHPParser_Node_Expr_PropertyFetch)
-            && $node->var instanceof \PHPParser_Node_Expr_New) {
-
+        if ((($node instanceof \PhpParser\Node\Expr\MethodCall
+                || $node instanceof \PhpParser\Node\Expr\PropertyFetch)
+                && $node->var instanceof \PHPParser_Node_Expr_New)
+            || ($node instanceof \PhpParser\Node\Expr\PropertyFetch &&
+            $node->var instanceof \PhpParser\Node\Expr\FuncCall)
+        ) {
             $tmp = new \PHPParser_Node_Expr_Variable('tmp');
             $assignment = new \PHPParser_Node_Expr_Assign($tmp, $node->var);
             $node->var = $tmp;
@@ -20,7 +23,8 @@ class ObjectInstAccess extends \PHPParser_NodeVisitorAbstract {
     }
 
     public function leaveNode(\PHPParser_Node $node) {
-        if (($node instanceof \PHPParser_Node_Expr_MethodCall || $node instanceof \PHPParser_Node_Expr_PropertyFetch)
+        if (($node instanceof \PHPParser_Node_Expr_MethodCall
+                || $node instanceof \PhpParser\Node\Expr\PropertyFetch)
             && $node->var instanceof \PHPParser_Node_Expr_Ternary) {
             $subject = clone $node;
             $subject->var = $node->var->if;
